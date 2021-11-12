67c30c9df8c3a441f8019823df4399af81b8da66
Ryahn <ryahn@Ryahns-MacBook-Pro.local>
2021-11-12 09:46:28 -0500

    Shard manager update
    Added comment on how its used

--- shard.js
+++ shard.js
@@ -3,11 +3,15 @@ const config = require("./config/config");
 const logger = require("./lib/logger/logger");
 const chalk = require("chalk");
 
+// Create sharding manager and pass in token
+// totalShards recommends using auto
 const manager = new ShardingManager("./index.js", {
     token: config.token,
     totalShards: "auto",
 });
 
+// Logs out [info] 12-11-2021 09:20:57: [SHARD] [Fri Nov 12 2021 09:20:57] Launched shard #0/1
+// TODO: Add tracking of each shard with status
 manager.on("shardCreate", async (shard) => {
     logger.info(
         `${chalk.blue("[SHARD]")} [${new Date()
@@ -15,12 +19,6 @@ manager.on("shardCreate", async (shard) => {
             .split(" ", 5)
             .join(" ")}] Launched shard #${shard.id}/${manager.totalShards}`
     );
-    // console.log(
-    //     `[SHARD] [${new Date()
-    //         .toString()
-    //         .split(" ", 5)
-    //         .join(" ")}] Launched shard #${shard.id}/${manager.totalShards}`
-    // );
 });
 
 manager.spawn();
