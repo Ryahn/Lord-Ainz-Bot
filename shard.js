a2c76c04cf2ff36ebef8fff944e70ae35ece52c9
Ryahn <ryancarr10@gmail.com>
2021-11-25 13:58:48 -0500

    Add docs

--- shard.js
+++ shard.js
@@ -3,15 +3,24 @@ const config = require('./config/config.json');
 const logger = require('./lib/logger/logger');
 const chalk = require('chalk');
 
-// Create sharding manager and pass in token
-// totalShards recommends using auto
+/**
+ * @name ShardManager
+ * @description Shard manager
+ * @param token requires discord token
+ * @param totalShards should be set to auto but can set max number
+ */
 const manager = new ShardingManager('./index-dev.js', {
     token: config.TOKEN,
     totalShards: 'auto',
 });
 
-// Logs out [info] 12-11-2021 09:20:57: [SHARD] [Fri Nov 12 2021 09:20:57] Launched shard #0/1
 // TODO: Add tracking of each shard with status
+
+/**
+ * @description logs out message on shard creation
+ * @yields [info] 12-11-2021 09:20:57: [SHARD] [Fri Nov 12 2021 09:20:57] Launched shard #0/1
+ * @todo Add tracking of each shard with status
+ */
 manager.on('shardCreate', async (shard) => {
     logger.info(
         `${chalk.blue('[SHARD]')} [${new Date()
